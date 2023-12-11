d70de686c72f714046818782620545e6b996c2b3
thomas.sun <sguoqing@gmail.com>
2023-12-11 15:11:52 +0800

    done day10

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 const KEY_CYCLE: [i32; 6] = [20, 60, 100, 140, 180, 220];
 fn main() {
@@ -34,19 +34,30 @@ fn main() {
             _ => (),
         }
     }
-    println!("{:?}", value_at);
+    // println!("{:?}", value_at);
     println!(
-        "{:?}",
+        "Part One:{:?}",
         value_at
             .into_iter()
             .fold(0, |acc, kv| { acc + kv.0 * kv.1 })
     );
-
-    //Part TWO
+    let part_two = part2();
+    part_two.chars().enumerate().for_each(|(i, c)| {
+        if i % 40 == 39 {
+            println!("{c}");
+        } else {
+            print!("{c}");
+        }
+    });
+    // println!("Part Two:{}", &part2()[..8]);
+}
+//Part TWO
+fn part2() -> String {
     let mut lines = include_str!("../data/input.txt").lines();
     let mut cycle = 0_i32;
-    let mut value = 1;
-    let mut screen = (0..240).map(|_| '.').collect::<Vec<_>>();
+    let mut sprite = 1;
+    let mut screen = HashSet::new();
+    // let mut row = 0;
     // screen.fill(b'.');
     while let Some(instr) = lines
         .next()
@@ -56,30 +67,35 @@ fn main() {
         match instr {
             ["noop"] => {
                 cycle += 1;
-                if KEY_CYCLE.contains(&cycle) {
-                    println!("{value},{:?}", value_at.insert(cycle, value));
+                // row = cycle % 40;
+                if (sprite - 1..=sprite + 1).contains(&((cycle - 1) % 40)) {
+                    // if (sprite - 1..=sprite + 1).contains(&(cycle - 1)) {
+                    screen.insert(cycle - 1);
                 }
             }
             ["addx", number] => {
                 cycle += 1;
-                if KEY_CYCLE.contains(&cycle) {
-                    value_at.insert(cycle, value);
+                // row = cycle % 40;
+                if (sprite - 1..=sprite + 1).contains(&((cycle - 1) % 40)) {
+                    // if (sprite - 1..=sprite + 1).contains(&(cycle - 1)) {
+                    screen.insert(cycle - 1);
+                    // screen.insert(cycle - 1);
                 }
                 cycle += 1;
-                if KEY_CYCLE.contains(&cycle) {
-                    value_at.insert(cycle, value);
+                // row = cycle % 40;
+                if (sprite - 1..=sprite + 1).contains(&((cycle - 1) % 40)) {
+                    screen.insert(cycle - 1);
+                    // screen.insert(cycle - 1);
                 }
 
-                value += number.parse::<i32>().unwrap();
+                sprite += number.parse::<i32>().unwrap();
             }
             _ => (),
         }
     }
-    println!("{:?}", value_at);
-    println!(
-        "{:?}",
-        value_at
-            .into_iter()
-            .fold(0, |acc, kv| { acc + kv.0 * kv.1 })
-    );
+    // println!("Part Two:{:?}", screen);
+
+    let mut output = std::iter::repeat('.').take(40 * 6).collect::<Vec<char>>();
+    screen.into_iter().for_each(|n| output[n as usize] = '#');
+    output.into_iter().collect::<String>()
 }
--- day-5/src/part2.rs
+++ day-5/src/part2.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, error::Error, fs};
+use std::{error::Error, fs};
 
 use regex::Regex;
 
