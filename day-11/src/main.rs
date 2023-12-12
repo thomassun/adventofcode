09318c9e870a1aabb8d84f29eda24a0610722a84
thomas.sun <sguoqing@gmail.com>
2023-12-12 20:55:51 +0800

    done part one of day11

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -1,45 +1,64 @@
 struct Monkey {
     bag: Vec<usize>,
-    op: Box<dyn FnOnce(usize) -> usize>,
+    op: Box<dyn Fn(usize) -> usize>,
     div: usize,
     if_to: usize,
     else_to: usize,
     ins: usize,
 }
 fn main() {
-    let lines = include_str!("../data/input.txt")
-        .lines()
+    let mut monkies = include_str!("../data/input.txt")
+        .split("\n\n")
+        .map(|m| {
+            let l: Vec<_> = m.lines().map(|l| l.split(": ").last().unwrap()).collect();
+            Monkey {
+                bag: l[1]
+                    .split(", ")
+                    .map(|n| n.parse::<usize>().unwrap())
+                    .collect::<Vec<_>>(),
+                op: {
+                    let expr = l[2].rsplit_once("= ").unwrap().1;
+                    op(expr)
+                },
+                div: l[3].rsplit_once(' ').unwrap().1.parse().unwrap(),
+                if_to: l[4].rsplit_once(' ').unwrap().1.parse().unwrap(),
+                else_to: l[5].rsplit_once(' ').unwrap().1.parse().unwrap(),
+                ins: 0,
+            }
+        })
         .collect::<Vec<_>>();
-    // .chunks(7);
-    lines.chunks(7).for_each(|ch| {
-        // let monkey = ch[0].parse::<i32>().unwrap();
-        let items_str = ch[1].split(&[':', ','][..]).collect::<Vec<_>>();
-        let items;
-        if let [_, end @ ..] = &items_str[..] {
-            items = end
-                .iter()
-                .map(|&x| x.trim().parse::<i32>().unwrap())
-                // .map(|&x| x)
-                .collect::<Vec<_>>();
-        };
-        // println!("{:?}", ch[1]);
-        let func = op(ch[2].split("new = ").collect::<Vec<_>>()[1]);
-        let next_func = next_step(&ch[3..]);
+
+    let mut bags = vec![vec![]; monkies.len()];
+    // let mo: usize = monkies.iter().map(|m| m.div).product();
+    (0..20).for_each(|_| {
+        monkies.iter_mut().enumerate().for_each(|(i, m)| {
+            m.bag.append(&mut bags[i]);
+            m.bag.drain(0..).for_each(|mut n| {
+                n = (m.op)(n) / 3;
+                bags[if n % m.div == 0 { m.if_to } else { m.else_to }].push(n);
+                m.ins += 1;
+            });
+        });
     });
+    monkies.sort_by(|a, b| b.ins.cmp(&a.ins));
+    println!(
+        "{}",
+        monkies.iter().take(2).map(|m| m.ins).product::<usize>()
+    );
 }
 
-fn op(expr: &str) -> Box<dyn FnOnce(i32) -> i32> {
+fn op(expr: &str) -> Box<dyn Fn(usize) -> usize> {
     let x = expr.split(' ').collect::<Vec<_>>();
     match x[..] {
         [_, op, "old"] => match op {
-            "+" => Box::new(|a| a + a),
+            "+" => Box::new(|a| a * 2),
             "-" => Box::new(|_a| 0),
             "*" => Box::new(|a| a * a),
             "/" => Box::new(|_a| 1),
             _ => panic!("Wrong op of the expression"),
         },
         [_, op, right] => {
-            let right_number = right.parse::<i32>().unwrap();
+            let right_number = right.parse::<usize>().unwrap();
             match op {
                 "+" => Box::new(move |a| a + right_number),
                 "-" => Box::new(move |a| a - right_number),
@@ -52,16 +71,6 @@ fn op(expr: &str) -> Box<dyn FnOnce(i32) -> i32> {
     }
 }
 
-fn next_step(expr: &[&str]) -> Box<dyn FnOnce(i32) -> i32> {
-    let number: i32 = expr[0].split("by ").collect::<Vec<_>>()[1].parse().unwrap();
-    let true_to: i32 = expr[1].split("monkey ").collect::<Vec<_>>()[1]
-        .parse()
-        .unwrap();
-    let else_to: i32 = expr[2].split("monkey ").collect::<Vec<_>>()[1]
-        .parse()
-        .unwrap();
-    Box::new(move |d| if d % number == 0 { true_to } else { else_to })
-}
 #[cfg(test)]
 mod tests {
     use super::*;
